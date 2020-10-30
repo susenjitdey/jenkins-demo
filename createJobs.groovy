dd0a42013b1384f41900a346191fe2f273c8c38e
SusenjitDey <SusenjitDey@DESKTOP-GR4UNS9>
2020-10-30 21:17:57 +0530

    UPdated
    Updated

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,14 @@
-pipelineJob('pipelineJob') {
+pipelineJob('theme-park-job') {
     definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/susenjitdey/fullstackgradle.git'
+                    }
+                    branch 'master'
+                }
+            }
         }
     }
 }
